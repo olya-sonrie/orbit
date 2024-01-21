b2d338031dc44bc500fbd3bb8e5036f1f83ed435
Olya Zaritska <blackskater23@gmail.com>
2024-01-21 14:29:41 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    extensions: ['.js','.ts', '.vue']
+  },
+  base: './',
+})
